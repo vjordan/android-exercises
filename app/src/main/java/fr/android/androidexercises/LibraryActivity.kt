84450f03333cd0fad2de3326eb68dd87524a3b05
Benjamin Lacroix <benjlacroix@gmail.com>
2018-11-04 17:46:14 +0100

    refactor(*): bump to gradle 4.10 and add kt support

---
+++ app/src/main/java/fr/android/androidexercises/LibraryActivity.kt
@@ -0,0 +1,41 @@
+package fr.android.androidexercises
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.Toolbar
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Button
+import android.widget.Toast
+
+class LibraryActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_library)
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        val openButton = findViewById<Button>(R.id.openButton)
+
+        openButton.setOnClickListener {
+            // Replace with startActivity() to start BookActivity
+            Toast.makeText(this@LibraryActivity, R.string.toast_todo, Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_library, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        val id = item.itemId
+
+        return if (id == R.id.action_settings) true else super.onOptionsItemSelected(item)
+    }
+}
