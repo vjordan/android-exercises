2631834e2f174188b62247082b9c9f04649d1dfe
vjordan <vincent.jordan@outlook.fr>
2018-11-16 10:11:05 +0100

    api

--- app/src/main/java/fr/android/androidexercises/HenriPotierService.kt
+++ app/src/main/java/fr/android/androidexercises/HenriPotierService.kt
@@ -1,5 +1,14 @@
 package fr.android.androidexercises
 
+import retrofit2.Call
+import retrofit2.http.GET
+
 interface HenriPotierService {
+
     // TODO Method GET books which return a List<Book>
+    interface Api {
+        @GET("books")
+        fun listBooks(): Call<Array<Book>>
+    }
+
 }
--- app/src/main/java/fr/android/androidexercises/LibraryActivity.kt
+++ app/src/main/java/fr/android/androidexercises/LibraryActivity.kt
@@ -2,6 +2,12 @@ package fr.android.androidexercises
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit.*
+import retrofit2.converter.gson.GsonConverterFactory
+import timber.log.Timber
 
 class LibraryActivity : AppCompatActivity() {
 
@@ -10,19 +16,39 @@ class LibraryActivity : AppCompatActivity() {
         setContentView(R.layout.activity_library)
 
         // Plant logger cf. Android Timber
-        // Timber.plant(new Timber.DebugTree());
+        Timber.plant(Timber.DebugTree())
+
 
         // TODO build Retrofit
+        val retrofit = Builder()
+                .baseUrl("http://henri-potier.xebia.fr/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
 
         // TODO create a service
+        val api = retrofit.create(HenriPotierService.Api::class.java)
 
         // TODO listBooks()
+        api.listBooks()
 
         // TODO enqueue call and display book title
+        api.listBooks().enqueue(object : Callback<Array<Book>> {
+            override fun onFailure(call: Call<Array<Book>>, t: Throwable) {
 
-        // TODO log books
+                Timber.e(t)
+            }
+
+            override fun onResponse(call: Call<Array<Book>>,
+                                    response: Response<Array<Book>>) {
+
+                // TODO log books
+                response.body()?.forEach { Timber.i(it.toString()) }
+
+            }
+        })
 
         // TODO display book as a list
+        
     }
 
 }
